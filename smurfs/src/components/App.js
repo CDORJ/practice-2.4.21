be9cbe56efc40d29fe439790235582ec31b7f95a
ORiveraJr84 <oriverajr84@gmail.com>
2021-02-05 02:28:31 -0500

    Got initial state working. Now working on adding

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,27 +1,46 @@
 import React, { Component } from "react";
-import {createStore, applyMiddleware} from "redux";
-import {Provider} from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import SmurfComponent from "./SmurfComponent";
 import SmurfForm from "./SmurfForm";
-import {rootReducer} from "../b-reducers";
-import "./App.css";
+import { rootReducer } from "../b-reducers";
+import { GOT_SMURFS, NO_SMURFS, website } from "../actions";
 
-const store = createStore(rootReducer,applyMiddleware(thunk, logger));
+import "./App.css";
+import axios from "axios";
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 class App extends Component {
+  componentDidMount() {
+    axios
+      .get(website)
+      .then((res) => {
+        store.dispatch({
+          type: GOT_SMURFS,
+          payload: res.data,
+        });
+      })
+      .catch((err) => {
+        store.dispatch({
+          type: NO_SMURFS,
+          payload: err.data,
+        });
+      });
+  }
   render() {
     return (
       <Provider store={store}>
-      	<div className="App">
-      	  <h1>SMURFS! W/Redux</h1>
-      	  <div>Welcome to your state management version of Smurfs!</div>
-      	  <div>Start inside of your `src/index.js` file!</div>
-      	  <div>Have fun!</div>
-          <SmurfForm/>
-          <SmurfComponent/>
-      	</div>
+        <div className="App">
+          <h1>SMURFS! W/Redux</h1>
+          <div>Welcome to your state management version of Smurfs!</div>
+          <div>Start inside of your `src/index.js` file!</div>
+          <div>Have fun!</div>
+          <SmurfForm />
+          <SmurfComponent />
+        </div>
       </Provider>
     );
   }
--- smurfs/src/components/SmurfComponent.js
+++ smurfs/src/components/SmurfComponent.js
@@ -5,30 +5,14 @@ import { connect } from "react-redux";
 import { getSmurfs } from "../actions";
 
 const SmurfComponent = (props) => {
-  const [initialSmurf, setInitialSmurf] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get(website)
-        .then((res) => {
-            console.log("res from axios in SmurfComponent", res)
-            setInitialSmurf(res.data)
-        })
-      .catch((err) => console.log(err));
-  }, []);
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    props.getSmurfs();
-  };
-
   return (
-      <div>
-          {initialSmurf.map((smurf) => (
-              <p>{smurf.name}</p>
-          ))}
-      "This is the Smurf Component"
-      <button onClick={handleClick}>GET SMURFS</button>
+    <div>
+      {props.smurfs.map((smurf) => (
+        <div class="card">
+          <h3>{smurf.name}</h3>
+          <p>{smurf.age}</p>
+        </div>
+      ))}
     </div>
   );
 };
